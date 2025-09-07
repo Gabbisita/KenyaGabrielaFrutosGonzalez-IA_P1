2a0de916a8b53b14704168e57ea020d756f6c908
Gabbisita <a23110189@live.ceti.mx>
2025-09-07 00:30:48 -0600

    Comentarios

--- Variables.py
+++ Variables.py
@@ -1,10 +1,19 @@
+#int o none sirven para no darle un  valor definido a nuestra variable.
 numero_1 = int()
 
 print(numero_1)
 
+#se pueden declarar valores de la siguiente manera siempre y cuando esten  bien formulados.
 a,b,c = 16,23,45
 print(a,b,c)
 
-valor_1 = 245
-valor_1 = 0
-print(valor_1)
+#se puede sobreponer el valor de una variable en otra.
+num = 245
+num = 0
+print(num) #escribira el ultimo
+
+
+#a,b,c = 16,23
+#print(a,b,c) 
+#comentar sirve para desactivar codigo
+
